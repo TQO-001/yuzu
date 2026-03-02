720a6d9fbf2b66dc5441860e8d4137b6feef3900
Thulani Langa <thulanilanga001@gmail.com>
2026-03-02 18:48:28 +0200

    (life): remove everything and start again | there's a lesson to be learnt here

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Strict mode helps catch bugs early in development
-  reactStrictMode: true,
+  /* config options here */
 };
 
 export default nextConfig;
